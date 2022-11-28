8ac413e6d497c334971f219be02074c097524b09
bogdannaida <bogdannaida@yandex.ru>
2022-11-28 04:27:51 +0300

    first commit

---
+++ app/models/article.rb
@@ -0,0 +1,14 @@
+class Article < ApplicationRecord
+  include Visible
+  
+  extend FriendlyId
+  
+  friendly_id :title
+
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+
+end
+
